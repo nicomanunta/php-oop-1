f7e7bf78c14f7f0446425493b18e50ddaa27c4a6
nicomanunta <nic.manun@gmail.com>
2024-01-30 14:10:52 +0100

    istanziati 3 oggetti

--- index.php
+++ index.php
@@ -5,5 +5,22 @@
         public $anno;
         public $regista;
     }
+    
+    $film_1 = new Movie();
+    $film_1 -> titolo = "The Prestige"
+    $film_1 -> genere = "thriller"
+    $film_1 -> anno = 2006
+    $film_1 -> regista = "Christopher Nolan"
 
+    $film_2= new Movie();
+    $film_2-> titolo = "Killers of the flower moon"
+    $film_2-> genere = "drammatico"
+    $film_2-> anno = "2023"
+    $film_2-> regista = "Martin Scorsese"
+
+    $film_3 = new Movie();
+    $film_3 -> titolo = "Chiedimi se sono felice"
+    $film_3 -> genere = "Commedia"
+    $film_3 -> anno = "2000"
+    $film_3 -> regista = "Massimo Venier"
 ?>
